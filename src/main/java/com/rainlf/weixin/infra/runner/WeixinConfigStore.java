fcd3a35812ec75ded8caa8daadd536d0bf581c20
Rain <10525928+rainlf@users.noreply.github.com>
2024-07-10 20:42:52 +0800

    update

--- src/main/java/com/rainlf/weixin/infra/runner/WeixinConfigStore.java
+++ src/main/java/com/rainlf/weixin/infra/runner/WeixinConfigStore.java
@@ -2,9 +2,8 @@ package com.rainlf.weixin.infra.runner;
 
 import com.rainlf.weixin.infra.db.entity.AppConfig;
 import com.rainlf.weixin.infra.db.repository.AppConfigRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -15,22 +14,23 @@ import java.util.Map;
  * @author rain
  * @date 6/23/2024 8:20 PM
  */
+@Slf4j
 @Component
-public class WeixinConfigStore implements ApplicationRunner {
+public class WeixinConfigStore {
     @Autowired
     private AppConfigRepository appConfigRepository;
 
     private final Map<String, String> configMap = new HashMap<>();
 
     public String getValue(String key) {
-        return configMap.get(key);
-    }
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        List<AppConfig> appConfigs = appConfigRepository.findAll();
-        for (AppConfig appConfig : appConfigs) {
-            configMap.put(appConfig.getKey(), appConfig.getValue());
+        if (configMap.isEmpty()) {
+            log.info("WeixinConfigStore, load config");
+            List<AppConfig> appConfigs = appConfigRepository.findAll();
+            for (AppConfig appConfig : appConfigs) {
+                log.debug("WeixinConfigStore, key: {}, value: {}", appConfig.getKey(), appConfig.getValue());
+                configMap.put(appConfig.getKey(), appConfig.getValue());
+            }
         }
+        return configMap.get(key);
     }
 }
