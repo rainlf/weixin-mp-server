45b27f881ae33bc36b527159ffa9a967ab7f4f71
hy何雨(IT) <yhee@trip.com>
2024-07-19 18:04:38 +0800

    1

--- src/main/java/com/rainlf/weixin/v3/infa/auth/JJwtUtil.java
+++ src/main/java/com/rainlf/weixin/v3/infa/auth/JJwtUtil.java
@@ -25,21 +25,21 @@ public class JJwtUtil {
     /**
      * 使用默认过期时间（7天），生成一个JWT
      */
-    public String createToken(String username) {
-        return createToken(username, null, defaultExpire);
+    public String createToken(String subject) {
+        return createToken(subject, null, defaultExpire);
     }
 
     /**
      * 生成token
      */
-    public String createToken(String username, Map<String, Object> claims, Long expire) {
+    public String createToken(String subject, Map<String, Object> claims, Long expire) {
         JwtBuilder builder = Jwts.builder();
         Date now = new Date();
         // 生成token
         builder.id(UUID.randomUUID().toString()) //id 这个可以不填，但是建议填
                 .issuer("Rain") //签发者
                 .claims(claims) //数据
-                .subject(username) //主题
+                .subject(subject) //主题
                 .issuedAt(now) //签发时间
                 .expiration(new Date(now.getTime() + expire)) //过期时间
                 .signWith(key); //签名方式
@@ -58,11 +58,11 @@ public class JJwtUtil {
                     .parseSignedClaims(token)
                     .getPayload();
         } catch (ExpiredJwtException e) {
-            throw new RuntimeException("token已过期");
+            throw new RuntimeException("token 已过期");
         } catch (JwtException e) {
-            throw new RuntimeException("token已失效");
+            throw new RuntimeException("token 已失效");
         } catch (Exception e) {
-            throw new RuntimeException("token解析失败");
+            throw new RuntimeException("token 解析失败");
         }
     }
 }
